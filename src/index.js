e875e8969bbfb7e8e225817d4159be88be495b4a
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-26 11:57:17 +0200

    ask gpt

--- src/index.js
+++ src/index.js
@@ -1,10 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { createStore } from 'redux';
+
+
+
+const initialState = 0;
+
+const reducer = (state = 0, action) => {
+ 
+  switch(action.type) {
+    case "INC": 
+    return state + 1;
+    case "DEC": 
+    return state - 1;
+    case "RND":
+      return state * action.payload;
+    default:
+      return state;
+  } 
+};
+
+const store = createStore(reducer);
+
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', payload: value});
+
+
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+};
+
+store.subscribe(update); 
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc())
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec())
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(value));
+});
+
+
+
+
+
+
+
+
+
+
+
+// let state = reducer(initialState, {type: 'INC'});
+//  state = reducer(state, {type: 'INC'});
+//  state = reducer(state, {type: 'INC'});
+//  state = reducer(state, {type: 'INC'});
+// console.log(state);
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <>
+   </>
   </React.StrictMode>
 );
